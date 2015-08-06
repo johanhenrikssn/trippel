2c2eb5ec497a1cb8dc310d1135b8da97a0b69a2e
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2015-08-06 20:59:34 +0200

    Some work on video-, and indexpage and added fontawesome to bower

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -7,7 +7,67 @@
 
     function mainController($scope) { 
 
-    	$scope.hej = "Stora fötter, stort hår";
+    	$scope.activeProject;
+        $scope.show = false;
+
+    	$scope.projects = [
+        {
+    		title: "POLYGON",
+    		description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#3E4F5B"
+        },
+        {
+            title: "MR.CARROT",
+            description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#B0B6BA"
+        },
+        {
+            title: "WEATHER",
+            description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#3BC0C3"
+        }, 
+        {
+            title: "MOTE",
+            description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#F16161"
+        }, 
+        {
+            title: "TRAIN",
+            description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#F0F0F1"
+        },
+        {
+            title: "X",
+            description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#3E4F5B"
+        },
+        {
+            title: "ANSIKTET",
+            description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#B0B6BA"
+        },
+        {
+            title: "JONKEN",
+            description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#3BC0C3"
+        },
+        {
+            title: "PUTTE",
+            description: "Nulla semper, metus vel consequat posuere, neque nisi molestie quam, eu rhoncus nibh ex sed eros. Ut condimentum nec risus nec interdum. Aenean eget lainia metus. In interdum mi eu nisi egestas, nec pulvinar felis accumsan. Quisque sodales luctus pellentesque. Etiam vulputate id sapien sed ultricies. Nulla ac lobortis libero. Suspendisse nec placerat eros, sed commodo lorem. Aliquam sem ante, elementum ac quam sed, condimentum bibendum dolor. Pellentesque imperdiet arcu ut orci vestibulum pretium. Nam maximus sem eget dolor eleifend condimentum. Quisque tempus eget arcu ac sagittis. Curabitur ac nibh imperdiet, hendrerit arcu non, mattis mauris. Phasellus a lectus sit amet nisl pulvinar sollicitudin. Nam sit amet augue sed nibh laoreet faucibus in et dolor. Aenean ex turpis, volutpat eu nibh in, semper sagittis dui.",
+            color: "#F16161"
+        }, 
+
+        ];
+
+
+
+
+
+        $scope.changeView = function(project) {
+
+            $scope.show = !$scope.show;
+            $scope.activeProject = project;
+        }
 
     }
 })();
