65e6bdf6b3319503e42102a252e7366ce6265a71
petraohlin8 <petraohlin8@gmail.com>
2015-09-10 22:09:21 +0200

    Added directive for carousel, added more projects, and so on..

---
+++ app/directives/carousel.directive.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('trippelApp')
+        .directive('carouselContainer', carouselContainer);
+
+    function carouselContainer() {
+        var directive = {
+            link: link,
+            templateUrl: 'app/views/carouselcontainer.html',
+            restrict: 'EA'
+        };
+
+        return directive;
+
+        function link($scope, $element, $attrs, $window) {
+          	
+            $(function() {
+                $('.carousel').carousel({
+                    interval: 3000
+                })
+            });     	
+
+        }
+
+
+    }
+
+})();
