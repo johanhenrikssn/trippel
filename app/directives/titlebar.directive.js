e1e1979a810e1b4b689c0510c2cc0a455232a58d
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2015-09-10 22:57:42 +0200

    Added a fixed titlebar with background on scroll

---
+++ app/directives/titlebar.directive.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('trippelApp')
+        .directive("titleBarScroll", hejhej);
+
+
+        function hejhej()
+        {
+            return function(scope, element, attrs) {
+                angular.element(window).bind("scroll", function() {
+
+                    if (!scope.scrollPosition) {
+                        scope.scrollPosition = 0;
+                    }
+                    
+                    var inWin = window.innerHeight;
+
+                    if (this.pageYOffset > 0.05*inWin) {
+                        scope.bgColor = "rgba(53,53,53,0.8)";
+                    } 
+                    else
+                        scope.bgColor = "transparent";
+
+                    scope.scrollPosition = this.pageYOffset;
+                    scope.$apply();
+                });
+            };
+        };
+
+})();
+
