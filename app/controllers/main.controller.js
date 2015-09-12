1f3212dbb88ff30d8678eec7aa343beb2153a26f
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2015-09-12 11:38:18 +0200

    Added exceptions for medieteknik-film, and description for Splash

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -10,35 +10,39 @@
 
         $http.get('projectdata.json').success(function(data) {
           $scope.projects = data;
-        });
+        }).then(function(){
 
-        $scope.activeProject;
-        $scope.show = false;
-        $scope.fullscreen = { bool: false};
+            $scope.activeProject;
+            $scope.show = false;
+            $scope.fullscreen = { bool: false};
 
-        $scope.infofilm = {
-            title: "Om medieteknik",
-            description: "",
-            source: "././projects/movies/presentationsfilm.mp4",
-            type: "video"
+            $scope.infofilm = {
+                title: "Om medieteknik",
+                description: "Här ligger tyngdpunkten på själva tekniken bakom medier och andra informationsflöden. Det kan handla om twitter, chat och smartphones, om dataspel och specialeffekter i film, om flygsimulatorer och tekniska verktyg för kirurger, det vill säga branscher och områden i ständig utveckling. Det är därför ingen slump att utbildningen sker i Norrköping, i nära anslutning till världsledande forskning och till Visualiseringscenter C.",
+                source: "././projects/movies/presentationsfilm.mp4",
+                type: "video"
 
-        };
-
-        $scope.changeView = function(project) {
-          $scope.show = !$scope.show;
-          $scope.activeProject = project;
-
-          $scope.fullscreen.bool = false;
-        };
-
-        $scope.getProjectsFromYear = function(year) {
-            var tmpArray = [];
-            for (var i = 0; i < $scope.projects.length; i++) {
-                if($scope.projects[i].year == year)
-                    tmpArray.push($scope.projects[i])
             };
 
-            return tmpArray;
-        };
+            $scope.changeView = function(project) {
+              $scope.show = !$scope.show;
+              $scope.activeProject = project;
+
+              $scope.fullscreen.bool = false;
+            };
+
+            $scope.getProjectsFromYear = function(year) {
+                var tmpArray = [];
+                for (var i = 0; i < $scope.projects.length; i++) {
+                    if($scope.projects[i].year == year)
+                        tmpArray.push($scope.projects[i])
+                };
+
+                return tmpArray;
+            };
+
+        });
+
+        
     }
 })();
